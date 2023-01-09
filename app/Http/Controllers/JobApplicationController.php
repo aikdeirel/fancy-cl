9b2cc6f993d89550c07b473508f59677a552d48a
Kai Riedel <kai.riedel@e-domizil.de>
2023-01-09 23:43:11 +0100

    add some models and migrations, set default lang to de and use models in main controller

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -2,19 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Applicant;
+use App\Models\ApplicantDetails;
+use App\Models\ApplicantSocials;
+use App\Models\ApplicantExperiences;
+use App\Models\ApplicantEducations;
 
 class JobApplicationController extends Controller
 {
-  public $applicant;
+  private $applicantDetails;
+  private $applicantSocials;
+  private $applicantExperiences;
+  private $applicantEducations;
 
   public function __construct()
   {
-    $this->applicant = Applicant::where('id', 1)->firstOrNew();
+    $this->applicantDetails = ApplicantDetails::get();
+    $this->applicantSocials = ApplicantSocials::get();
+    $this->applicantExperiences = ApplicantExperiences::get();
+    $this->applicantEducations = ApplicantEducations::get();
   }
 
   public function cv()
   {
-    dd($this->applicant);
+    return view('cv', [
+      'applicantDetails' => $this->applicantDetails,
+      'applicantSocials' => $this->applicantSocials,
+      'applicantExperiences' => $this->applicantExperiences,
+      'applicantEducations' => $this->applicantEducations,
+    ]);
   }
 }
---
+++ database/migrations/2023_01_09_213923_applicant_experiences.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicant_experiences', function (Blueprint $table) {
+            $table->id();
+            $table->string('experience_title');
+            $table->string('experience_company');
+            $table->date('experience_from');
+            $table->date('experience_to');
+            $table->text('experience_description');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
